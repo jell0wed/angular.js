b8bd642142c5cc76c06cd5b19b0e5b7a69de0e5a
Jeremie Poisson <jepoisso@microsoft.com>
2018-10-26 16:09:29 -0700

    fix

--- src/ng/qPromiseTracker.js
+++ src/ng/qPromiseTracker.js
@@ -6,6 +6,10 @@
  * @description
  * Tracks active promises by tracking and untracking promises as they are created,
  * rejected or resolved. 
+ * 
+ * By default, this does not track any promises that gets created.
+ * To use this API, one must override the `$$qPromiseTracker` at config time
+ * and provide their own tracking implementation using `$provide`.
  *
  */
 function $$QPromiseTrackerProvider() {
